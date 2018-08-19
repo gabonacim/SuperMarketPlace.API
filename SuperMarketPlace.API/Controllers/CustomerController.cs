1a9a65fcfda1e8b5e3b3515a94bfff6199b20928
Gabriel Bonacim <gabriel.bonacim@dartdigital.com.br>
2018-08-19 02:05:30 -0300

    Remoção de código comentado

--- SuperMarketPlace.API/Controllers/CustomerController.cs
+++ SuperMarketPlace.API/Controllers/CustomerController.cs
@@ -1,10 +1,6 @@
 ﻿using SuperCustomerPlace.Repository.Repository;
 using SuperMarketPlace.Repository.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace SuperCustomerPlace.API.Controllers
@@ -17,51 +13,6 @@ namespace SuperCustomerPlace.API.Controllers
         {
             repository = new CustomerRepository();
         }
-
-        //public IEnumerable<Customer> Get()
-        //{
-        //    return repository.GetAll();
-        //}
-
-        //public Customer Get(long id)
-        //{
-        //    var customer = repository.Get(id);
-
-        //    if (customer == null)
-        //        throw new HttpResponseException(HttpStatusCode.NotFound);
-
-        //    return customer;
-        //}
-
-        //public void Post([FromBody]Customer value)
-        //{
-        //    if (value == null)
-        //    {
-        //        throw new HttpResponseException(HttpStatusCode.BadRequest);
-        //    }
-
-        //    repository.Add(value);
-        //}
-
-        //public void Put(int id, [FromBody]Customer value)
-        //{
-        //    if (id <= 0 || value == null)
-        //    {
-        //        throw new HttpResponseException(HttpStatusCode.NotFound);
-        //    }
-
-        //    repository.Update(value);
-        //}
-
-        //public void Delete(long id)
-        //{
-        //    if (id <= 0)
-        //    {
-        //        throw new HttpResponseException(HttpStatusCode.NotFound);
-        //    }
-
-        //    repository.Delete(id);
-        //}
 
         [HttpPost]
         [Route("api/customer/login")]
